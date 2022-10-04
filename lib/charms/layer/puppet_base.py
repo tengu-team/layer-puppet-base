73eabe8a4528c9491ad3d00793ee0a6fb38f083c
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 05:19:28 +0000

    Adding numeric_owner as keyword arguement

--- lib/charms/layer/puppet_base.py
+++ lib/charms/layer/puppet_base.py
@@ -108,7 +108,7 @@ class Puppet:  # pylint:disable=R0902
                         if not is_within_directory(path, member_path):
                             raise Exception("Attempted Path Traversal in Tar File")
                 
-                    tar.extractall(path, members, numeric_owner) 
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
                     
                 
                 safe_extract(tar, path="files/puppet")
